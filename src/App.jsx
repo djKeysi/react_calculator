f3031f77618fd65ed091b8e942eae126edb48218
Зенченко Евгений <zenchenko_es@outlook.com>
2025-02-23 23:43:47 +0700

    add raschet

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,15 @@ export const App = () => {
 	const [equally, setEqually] = useState('');
 
 	let [rezult, setRezult] = useState('');
+	let [valueInput, setvalueInput] = useState('');
 
 	const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	const handle = (event) => {
+		//return e.target.value;
+		//setOperand1(e.target.value);
+		//setvalueInput(event.target.value);
+		console.log('sdsd', valueInput);
+	};
 
 	return (
 		<div className={styles.parent}>
@@ -21,12 +28,23 @@ export const App = () => {
 				<h1>Калькулятор</h1>
 				<input
 					className={styles.input}
-					disabled={true}
+					disabled={false}
 					value={
-						equally !== ''
-							? rezult //+ operator + operand2
-							: operand1 + operator + operand2 + equally
+						//	valueInput.trim()
+						equally === ''
+							? (valueInput = operand1 + operator + operand2)
+							: operator === '-'
+								? (valueInput = Number(operand1) - Number(operand2))
+								: operator === '+'
+									? (valueInput = Number(operand1) + Number(operand2))
+									: equally !== ' '
+										? (valueInput = 'ssssss')
+										: '1111'
+						// operator === '+'
+						// 	? operand1 + operator + operand2
+						// 	: operand2 + operator + operand1
 					}
+					onChange={handle}
 				/>
 				<div>
 					{' '}
@@ -34,6 +52,16 @@ export const App = () => {
 						className={styles.btn_block}
 						onClick={() => {
 							setOperator('-');
+							setEqually('');
+
+							if (equally !== '') {
+								setOperand1(valueInput);
+								setOperand2('');
+								// valueInput = operator;
+								// console.log(valueInput);
+
+								// setvalueInput(valueInput);
+							}
 						}}
 					>
 						-
@@ -41,6 +69,16 @@ export const App = () => {
 					<button
 						onClick={() => {
 							setOperator('+');
+							setEqually('');
+
+							if (equally !== '') {
+								setOperand1(valueInput);
+								setOperand2('');
+								// valueInput = operator;
+								// console.log(valueInput);
+
+								// setvalueInput(valueInput);
+							}
 							// setOperand1('');
 							// setOperand2('');
 							// setOperator('');
@@ -52,6 +90,7 @@ export const App = () => {
 						className={styles.btn_block}
 						onClick={() => {
 							setEqually('=');
+							//setRezult(rezult);
 						}}
 					>
 						=
@@ -71,34 +110,73 @@ export const App = () => {
 						{nums.map((num, index) => (
 							<button
 								onClick={() => {
+									// operand1 += num;
+									// setOperand1(operand1);
+									// if (operator === '-') {
+									// 	operand2 += num;
+									// 	setOperand2(operand2);
+									// 	//setOperator('');
+									// }
 									if (operator === '-' || operator === '+') {
 										operand2 += num;
 										setOperand2(operand2);
+
+										setvalueInput(operand2);
+
+										//setOperator('');
 									} else {
 										operand1 += num;
 										setOperand1(operand1);
+
+										setvalueInput(operand1);
+
+										//setOperator('');
 									}
 
-									switch (operator) {
-										case '-':
-											rezult = Number(operand1) - Number(operand2);
-											setRezult(rezult);
+									//	setEqually('');
 
-											break;
-										case '+':
-											rezult = Number(operand1) + Number(operand2);
-											setRezult(rezult);
-											break;
-									}
+									//console.log(valueInput + operator);
 
+									// setRezult(rezult);
+									// if (rezult !== '') {
+									// 	console.log('ddd', rezult);
+									// }
+
+									//
+									// num += operand1;
+									// operand2 += num;
+									// setOperand1(num);
+									// //	console.log(operand2);
+									// switch (operator) {
+									// 	case '-':
+									// 		rezult = Number(operand1) - Number(operand2);
+									// 		setRezult(rezult);
+									// 		//setOperator('');
+									// 		setOperand2(num);
+									// 		break;
+									// 	case '+':
+									// 		rezult = Number(operand1) + Number(operand2);
+									// 		setRezult(rezult);
+									// 		break;
+									// 	// default:
+									// 	// 	setOperator('');
+									// }
+									// if (rezult !== '') {
+									// 	//setRezult('');
+									// 	//setOperator('');
+									// 	// setOperand1('');
+									// 	// setOperand2('');
+									// } else {
+									// 	setOperator('');
+									// }
+									//}
 									// if (rezult === '') {
 									// 	setOperator('');
 									// }
-
-									console.log(
-										'operand',
-										operand1 + operator + operand2 + rezult,
-									);
+									// console.log(
+									// 	'operand',
+									// 	operand1 + operator + operand2 + rezult,
+									// );
 								}}
 								key={index}
 							>
